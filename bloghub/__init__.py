f5880c4b21efea0eeeec966f101721d286615d2d
Tyra-hans <tyrahans17@gmail.com>
2019-10-27 19:10:40 +0300

    creates validation that checks if user already exists or if email is already taken inorder to avoid integrity errors. Also adds user loader and login manager to help manage thr login sessions

--- bloghub/__init__.py
+++ bloghub/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY']= '1371ce8114eac4891b9b92bcea6ecd46'
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from bloghub import views
