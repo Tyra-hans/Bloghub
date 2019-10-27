a677f1b307abb64edd58f2cc30644106e7d25dd3
Tyra-hans <tyrahans17@gmail.com>
2019-10-27 10:22:12 +0300

    adds initial html structures and routing between home and about pages. also imports forms and adds a registration form that will allow users to input data for registration

---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(),Length(min=2, max=20)])
+
+    
