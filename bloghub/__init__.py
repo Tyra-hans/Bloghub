263ea7bb33380a650db94723098eefb22f6d4fdb
Tyra-hans <tyrahans17@gmail.com>
2019-10-27 18:56:06 +0300

    adds users to the database on registration from the form input data

--- bloghub/__init__.py
+++ bloghub/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY']= '1371ce8114eac4891b9b92bcea6ecd46'
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from bloghub import views
