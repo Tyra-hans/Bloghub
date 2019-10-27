87a2b66009a4fb3624d555471a69954ac358b114
Tyra-hans <tyrahans17@gmail.com>
2019-10-27 10:34:31 +0300

    adds secret key for security purposes and creates registration and login form that allows users to use the forms to input data required

--- bloghub.py
+++ bloghub.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+app.config['SECRET_KEY']= '1371ce8114eac4891b9b92bcea6ecd46'
+
+posts = [
+    {
+        'author': 'Tyra Hans',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Ren Oyuo',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 
 @app.route("/")
 def home():
