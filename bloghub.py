a6713e5648995bc9a840a1ba2d6aec840ca7c339
Tyra-hans <tyrahans17@gmail.com>
2019-10-27 10:38:36 +0300

    creates route functions for registration and login that will return the desired template

--- bloghub.py
+++ bloghub.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY']= '1371ce8114eac4891b9b92bcea6ecd46'
@@ -27,6 +28,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+form= RegistrationForm
+def about():
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+form= LoginForm
+def about():
+    return render_template('Login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
